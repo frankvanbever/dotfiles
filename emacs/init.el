ef4c71a8b89fe67d6ee58dae254a8cc35541aab6
Frank Vanbever <frank.vanbever@gmail.com>
2014-10-22 10:23:59 +0200

    major emacs init file cleanup

--- emacs/init.el
+++ emacs/init.el
@@ -1,11 +1,13 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Emacs 24 Personal configuration
-;; Frank Vanbever - frank.vanbever@gmail.com
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Emacs 24 Personal configuration           ;;
+;; Frank Vanbever - frank.vanbever@gmail.com ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Packaging setup
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list  'load-path "~/.emacs.d/lisp/")
+
+;;;;;;;;;;;;;;;;;;;;;;
+;; Packaging setup  ;;
+;;;;;;;;;;;;;;;;;;;;;;
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 			 ("marmelade" . "http://marmelade-repo.org/packages/")
@@ -13,168 +15,48 @@
 
 (package-initialize)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; inhibit splash screen 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq inhibit-splash-screen t)
+;;;;;;;;;;;;;;
+;; Settings ;; 
+;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; EVIL mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq inhibit-splash-screen t) ;; inhibit splash screen 
+(global-linum-mode 1) ;; enable global line number mode
+(nyan-mode 1) ;; enable nyan-mode
+(load-theme 'solarized-dark t) ;; load Solarized dark theme
 
-;; enable EVIL mode
-(require 'evil)
-(evil-mode 1)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable IDO mode
-;; http://www.masteringemacs.org/articles/2010/10/10/introduction-to-ido-mode/
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(setq ido-create-new-buffer 'always) ;;don't ask for permission to create a new buffer
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-;; enable IDO-ubiquitous
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; EVIL mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; enable EVIL mode
-(require 'evil)
-(evil-mode 1)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable IDO mode
-;; http://www.masteringemacs.org/articles/2010/10/10/introduction-to-ido-mode/
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(setq ido-create-new-buffer 'always) ;;don't ask for permission to create a new buffer
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-;; enable IDO-ubiquitous
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; EVIL mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; enable EVIL mode
-(require 'evil)
-(evil-mode 1)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable IDO mode
-;; http://www.masteringemacs.org/articles/2010/10/10/introduction-to-ido-mode/
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(setq ido-create-new-buffer 'always) ;;don't ask for permission to create a new buffer
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-;; enable IDO-ubiquitous
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; EVIL mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; enable EVIL mode
-(require 'evil)
-(evil-mode 1)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable IDO mode
-;; http://www.masteringemacs.org/articles/2010/10/10/introduction-to-ido-mode/
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(setq ido-create-new-buffer 'always) ;;don't ask for permission to create a new buffer
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-;; enable IDO-ubiquitous
-(require 'ido-ubiquitous)
-(ido-ubiquitous-mode 1)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable SMEX mode
-;; https://github.com/nonsequitur/smex/  
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(require 'smex)
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "<menu>") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-		
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; load Solarized dark theme
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(load-theme 'solarized-dark t)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable global line number mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(global-linum-mode 1)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable autopair mode 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (require 'autopair)
-;; (autopair-global-mode) ;; enable autopair in all buffers
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable yasnippet mode 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;(require 'yasnippet)
-;;(yas/load-directory "~/.emacs.d/snippets")
-;;(yas/global-mode 1)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable autocomplete mode 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(require 'auto-complete)
+(require 'auto-complete) ;; enable autocomplete mode 
 (global-auto-complete-mode)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable nyan-mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(nyan-mode 1)
+;;;;;;;;;;;;;;;
+;; Evil Mode ;;
+;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; enable rainbow delimiter mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'evil) 
+(evil-mode 1) ;; enable EVIL mode
 
-(require 'rainbow-delimiters)
+;; change cursor color depending on Evil mode 
+;; colors modified for solarized dark
+(setq evil-emacs-state-cursor '("#dc322f" box))
+(setq evil-normal-state-cursor '("#859900" box))
+(setq evil-visual-state-cursor '("#cb4b16" box))
+(setq evil-insert-state-cursor '("#dc322f" bar))
+(setq evil-replace-state-cursor '("#dc322f" bar))
+(setq evil-operator-state-cursor '("#dc322f" hollow))
 
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; Rainbow delimiters ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'rainbow-delimiters) 
 (defun rainbowDelimiter-hook ()
   (rainbow-delimiters-mode 1))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; C mode customisations 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;
+;; C ;;
+;;;;;;;
 
 ;; automatically indent C code
 (defun c-autoindent-hook ()
@@ -184,30 +66,18 @@
 ;; enable rainbow delimiters
 (add-hook 'c-mode-hook 'rainbowDelimiter-hook)
 
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Emacs Lisp mode customisations
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;
+;; ELisp ;;
+;;;;;;;;;;;
 
 ;; enable rainbow delimiters
 (add-hook 'emacs-lisp-mode-hook  'rainbowDelimiter-hook)
 
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; set pdflatex as default
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;
+;; LaTeX ;;
+;;;;;;;;;;;
 
 (setq TeX-PDF-mode t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; disable yas for term modes 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(add-hook 'term-mode-hook (lambda()
-        (setq yas-dont-activate t)))
 
 ;; HERE BE DRAGONS (and Custom modufications, do not touch)
 
@@ -224,3 +94,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
+(put 'dired-find-alternate-file 'disabled nil)
